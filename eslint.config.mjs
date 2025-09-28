b23be72423c83f32a2e4e8128d77d3e28b1786fc
Chelsea.Y <yangqingyan0@gmail.com>
2025-09-28 14:56:41 +1000

    fix: add ignore prisma generated

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,16 +12,13 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    rules: {
-      "@typescript-eslint/no-require-imports": "off",
-      "@typescript-eslint/no-this-alias": "off",
-    },
     ignores: [
       "node_modules/**",
       ".next/**",
       "out/**",
       "build/**",
       "next-env.d.ts",
+      "src/generated/**",
     ],
   },
 ];
